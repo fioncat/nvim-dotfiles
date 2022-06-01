92b6791056b9670bb88d851a85753e04ec87d14a
user <user@FVFF878GQ6LR.local>
2022-06-01 23:46:55 +0800

    add install

---
+++ install.sh
@@ -0,0 +1,13 @@
+if [ -d $HOME/.conf/nvim ]; then
+	rm -rf $HOME/.conf/nvim-back
+fi
+if [ -d $HOME/.local/share/nvim/site ]; then
+	rm -rf $HOME/.local/share/nvim/site
+fi
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone https://github.com/fioncat/nvim-dotfiles.git $HOME/.config/nvim
+
+NVM_HOME=$HOME/.config/nvim
+mv $NVM_HOME/init.lua $NVM_HOME/init-back.lua
+mv $NVM_HOME/init-install.lua $NVM_HOME/init.lua
